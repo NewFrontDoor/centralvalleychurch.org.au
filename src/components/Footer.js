67d178eaef54690c1188038dadfc53973b9dc0cf
Bratalix <bratalix@gmail.com>
2019-03-15 12:26:16 +1100

    Updating react-scripts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -34,7 +34,7 @@ class Footer extends Component {
                     <div className="content">
                       <div className="footer-mail text-center">
                         <i className="icon ion-ios7-email-outline size-32 margin-bottom-20"></i>
-                        <p><a href="mailto:">Contact Email</a><br /></p>
+                        <p><a href="mailto:info@centralvalleychurch.org.au">info@centralvalleychurch.org.au</a><br /></p>
                       </div> {/* /footer-mail */}  </div>
                   </div>  </div>
               </div> {/* /footer-second-column-region */}
