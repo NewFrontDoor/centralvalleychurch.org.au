bdf79c294e3146fdebd722f8a1765269104f011f
Bratalix <bratalix@gmail.com>
2019-10-04 12:03:57 +1000

    Moving all content only pages to API

--- src/components/OtherPageWrapper.js
+++ src/components/OtherPageWrapper.js
@@ -1,6 +1,5 @@
-/* eslint-disable */
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; //eslint-disable-line
 
 import OtherPageContent from './OtherPageContent';
 
@@ -8,24 +7,12 @@ import AllSermons from './pages/AllSermons';
 import Sermons from './pages/Sermons';
 import SermonPage from './pages/SermonPage';
 import SermonSeriesPage from './pages/SermonSeriesPage';
-import GospelCommunities from './pages/GetInvolved/GospelCommunities';
 
 
-import OurPeople from './pages/OurPeople';
-
+//import OurPeople from './pages/OurPeople';
 
 import ContactUs from './pages/ContactUs';
-import PrayerMeetings from './pages/GetInvolved/PrayerMeetings';
-import MainlyMusic from './pages/GetInvolved/MainlyMusic';
-import LifeCourse from './pages/GetInvolved/LifeCourse';
-import Vision from './pages/AboutUs/Vision';
-import Team from './pages/AboutUs/Team';
-import Mission from './pages/AboutUs/Mission';
-import WhatWeBelieve from './pages/AboutUs/WhatWeBelieve';
-import VisitUs from './pages/VisitUs';
-import WelcomeAbout from './pages/ImNew/WelcomeAbout';
-import WhoIsJesus from './pages/ImNew/WhoIsJesus';
-import WhyCome from './pages/ImNew/WhyCome';
+
 import GenericAPIPage from './pages/GenericAPIPage';
 
 
@@ -41,26 +28,10 @@ class OtherPageWrapper extends Component {
           <Route exact path="/series/:nid" component={SermonSeriesPage} />
           <Route exact path="/series/:nid/:title" component={SermonSeriesPage} />
 
-          <Route exact path="/OurPeople" component={OurPeople} />
           <Route exact path="/ContactUs" component={ContactUs} />
 
-
-          <Route exact path="/VisitUs" component={VisitUs} />
-
-          <Route exact path="/PrayerMeetings" component={PrayerMeetings} />
-          <Route exact path="/GospelCommunities" component={GospelCommunities} />
-          <Route exact path="/LifeCourse" component={LifeCourse} />
-
-          <Route exact path="/Welcome" component={WelcomeAbout} />
-          <Route exact path="/Vision" component={Vision} />
-          <Route exact path="/Team" component={Team} />
-          <Route exact path="/Mission" component={Mission} />
-          <Route exact path="/WhatWeBelieve" component={WhatWeBelieve} />
-
-          <Route exact path="/WhoIsJesus" component={WhoIsJesus} />
-          <Route exact path="/WhyCome" component={WhyCome} />
-          
           <Route exact path="/:slug" component={GenericAPIPage} />
+
           <Route path="/*" component={OtherPageContent} />
         </Switch>
       </section>
--- src/components/pages/GenericAPIPage.js
+++ src/components/pages/GenericAPIPage.js
@@ -1,25 +1,24 @@
-/* eslint-disable */
 import React, { Component } from 'react';
-import {getPageContent } from '../../utils/fetchJSON';
+import { getPageContent } from '../../utils/fetchJSON';
 
 
 class GenericAPIPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          pages: null,
-          slug: this.props.match.params.slug,
-          pageExists: false,
-          loading: true
+            pages: null,
+            slug: this.props.match.params.slug,
+            pageExists: false,
+            loading: true
         }
-      }
-    
-      componentWillMount() {
+    }
+
+    componentWillMount() {
         var that = this;
-    
+
         getPageContent(this.state.slug, function (data) {
-          that.setState({ pages: data, loading: false });
-          data.length>0 ? that.setState({pageExists: true}) : that.setState({pageExists : false});
+            that.setState({ pages: data, loading: false });
+            data.length > 0 ? that.setState({ pageExists: true }) : that.setState({ pageExists: false });
         });
     }
 
@@ -27,22 +26,35 @@ class GenericAPIPage extends Component {
         var pageHeader = "";
         var content = "";
         var page = null;
-        if(this.state.pages !== null && this.state.pages.length > 0){
+        if (this.state.pages !== null && this.state.pages.length > 0) {
             //page exists
             page = this.state.pages[0];
-            pageHeader = page.header_image ? <img className="img img-responsive" src={page.header_image} alt="" ></img> : <h1>{page.title}</h1>;
+            pageHeader = page.header_image ? <img className="img img-responsive" src={page.header_image} alt="" ></img> : (
+                <section>
+                    <div id="top-content-left-region" className="padding-top-15 padding-bottom-15 top-content-left col-xs-12 col-md-6 text-center-sm">
+                        <div id="page-title-block" className="page-title block">
+                            <h1>{page.title}</h1>
+                        </div>
+                    </div>
+
+                    <div id="top-content-right-region" className=" padding-top-15 top-content-right col-xs-12 col-md-6 text-right text-center-sm">
+                        <div id="page-breadcrumbs-block" className="page-breadcrumbs block">
+                            {page.breadcrumbs ? <div className="breadcrumbs" dangerouslySetInnerHTML={{ __html: page.breadcrumbs }} /> : <div className="breadcrumbs"><a href="/">Home</a></div>}
+                        </div>
+                    </div>
+                </section>);
             content = page.content;
         }
-        else if(this.state.pages !== null){
+        else if (this.state.pages !== null) {
             //no page
             content = <p>Page could not be found.</p>
             pageHeader = (<div id="top-content-left-region" className="top-content-left col-xs-12 col-md-6 text-center-sm">
-            <div id="page-title-block" className="page-title block">
-              <h1>Page not found</h1>
-            </div></div>)
+                <div id="page-title-block" className="page-title block">
+                    <h1>Page not found</h1>
+                </div></div>)
             console.log(this.state);
         }
-        else{
+        else {
             //loading
             content = <p>Loading...</p>
         }
@@ -62,16 +74,14 @@ class GenericAPIPage extends Component {
                                 <div className="region region-content">
                                     <div id="block-system-main" className="block block-system">
                                         <div className="content">
-                                            <div className="node node-page clearfix">
-                                                
-                                                {this.state.loading === false && this.state.pageExists ? <div className="content" dangerouslySetInnerHTML={{__html: content}}></div> : ''}
-                                                {this.state.loading === true ? <div className="content">Loading</div> : ''}
-                                                {this.state.loading === false && this.state.pageExists === false ? <div className="contetnt">{content}</div> : ''}
 
+                                            {this.state.loading === false && this.state.pageExists ? <div className="content" dangerouslySetInnerHTML={{ __html: content }}></div> : ''}
+                                            {this.state.loading === true ? <div className="content">Loading</div> : ''}
+                                            {this.state.loading === false && this.state.pageExists === false ? <div className="contetnt">{content}</div> : ''}
 
-                                            </div>
                                         </div>
-                                    </div>  </div>
+                                    </div>
+                                </div>
 
 
 
