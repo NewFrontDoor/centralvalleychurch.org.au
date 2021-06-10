8494ac4fce149c85fc5580bac1e84fd6b123b4a5
Bratalix <bratalix@gmail.com>
2021-06-10 10:32:20 +1000

    Requested update

--- src/components/homepage/Welcome.js
+++ src/components/homepage/Welcome.js
@@ -25,6 +25,9 @@ class Welcome extends Component {
                               <h3 className="text-center">Sundays 10am</h3>
                               <p>We are group of ordinary people from all walks of life who are being changed by and through a relationship with Jesus. We meet together on Sundays to hear, explore and unpack what the Bible has to say about life. We love to meet new people who are interested in finding out more about Jesus and would love to welcome you and get to know you.</p>
                             </div>
+                            <div className="text-center margin-top-10">
+                              <a href="https://www.youtube.com/watch?v=-Zsh66MdcrI" target="_blank" rel="noreferrer noopener"><button className="btn btn-primary">How to get to CVC?</button></a>
+                            </div>
                           </div></div>  </div>
 
                       </div>
